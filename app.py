51a407e6c05a1f111ac2591be904657c1fc7ac47
solominh <hoangminhftu2@gmail.com>
2017-01-12 00:50:40 +0700

    Fix stop a thread

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from extract_text_from_image import extract_text_from_blackwhite_screen_region
 from keyboard_utils import send_keys_from_file
 import time
 import keyboard
+import threading
 
-is_starting = False
+
 bbox = None
 
 
 def main():
-    global bbox, is_starting
+    global bbox,  start_hotkey
 
     # Specify bbox
     bbox = specify_image_bbox()
@@ -19,37 +20,31 @@ def main():
     print('bbox = ' + str(bbox))
 
     # Show guide
-    register_hotkey()
+    start_hotkey = keyboard.add_hotkey('ctrl+alt+shift+k', handle_start_hotkey)
     print('Start listening to hotkey')
     print('Press Ctrl + Alt + Shift + K to start')
     print('Or Press ESC to stop listening to hotkey')
 
     keyboard.wait('esc')
-    is_starting = False
+    print('Hacking stop')
 
 
-def handle_start_hotkey():
-    global is_starting, start_hotkey
-
-    keyboard.remove_hotkey(start_hotkey)
+def start_hacking():
     print('Start hacking')
     print('Press ESC to stop')
 
-    is_starting = True
-
-    while is_starting:
+    while True:
         text_filepath = extract_text_from_blackwhite_screen_region(bbox)
         send_keys_from_file(text_filepath)
         time.sleep(2)
 
-    print('Stop hacking')
+    print('Finish stopping hacking')
 
 
-def register_hotkey():
-    global start_hotkey
-
-    start_hotkey = keyboard.add_hotkey(
-        'ctrl+alt+shift+k', handle_start_hotkey, blocking=True, timeout=2)
+def handle_start_hotkey():
+    keyboard.remove_hotkey(start_hotkey)
+    t = threading.Thread(target=start_hacking)
+    t.start()
 
 
 main()
